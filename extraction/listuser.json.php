cae2a5018e35badbc8066b54b0c411d0869a8003
Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
2013-06-02 16:45:42 +0200

    Fix last comma in json file

--- extraction/listuser.json.php
+++ extraction/listuser.json.php
@@ -1,18 +1,14 @@
-<?php
-
-require 'lib/IntersectionDAO.php';
-
-$users = IntersectionDAO::getFacebookUsers();
-
-?>
+<?php require 'lib/IntersectionDAO.php'; ?>
 
 {
-<?php foreach($users as $user): ?>
-  <?php sprintf("%s: '%s' '%s'",
-  $user['facebook_user_id'],
-  $user['last_name'],
-  $user['first_name']
-  );
-  ?>
-<?php end foreach; ?>
+    <?php $users = IntersectionDAO::getFacebookUsers(); ?>
+    <?php foreach($users as $k => $user): ?>
+        <?php printf("'%s': '%s %s'%s\n",
+            $user['facebook_user_id'],
+            $user['last_name'],
+            $user['first_name'],
+            $k == count($users) - 1 ? '' : ','
+            );
+        ?>
+    <?php endforeach; ?>
 }
