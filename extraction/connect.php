20dd532c09eb5e841ea47a56e1ff41c335fb51a3
rafinette <RAFINETTE@wiportucb853.univ-lyon1.fr>
2013-06-03 09:41:48 +0200

    add connect.php

--- extraction/connect.php
+++ extraction/connect.php
@@ -1,20 +1,60 @@
-<meta charset="utf-8" />
 <html>
-  <head>
-    <title>Intersection</title>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-  </head>
-  
-  <body>
+<!DOCTYPE html>
 
-    <h1>Pour utiliser l'appli intersection connectez-vous à votre compte Facebook</h1>
-    <h2>Page Facebook de l'appli: <a href="https://www.facebook.com/aspi.kirby">Aspi Kirby local</a></h2>
+	<head>
+		<meta charset="utf-8">
+		<title>Intersection</title>
+		<link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+		<link rel="stylesheet" href="bootstrap/css/bootstrap-responsive.css">	
+	</head>
 
-    <p>Se connecter via Facebook</p>
-    <br />
-    <a href="<?php echo $app->getFacebookLoginUrl(); ?>">
-      <img src="pictures/facebook-connect-logo.jpg">
-    </a>
+	<body>
+		<div class="container-narrow">
+			<div class="masthead">
+				<h3 class="muted">Intersection</h3>
+			</div>
+			<hr>
+			<div class="presentation">
+			<center>
+				<h1> Facebook Cross Visualization</h1>
+					<p class="lead">Intersection is a facebook application that allows you to
+				 	view mutual friends of two users and also all of them.</br>
+				 	</p>
+					<a href="<?php echo $app->getFacebookLoginUrl(); ?>">
+					<img src="pictures/facebook-connect-logo.jpg"></a>
+				</center>
+			</div>
+			<hr>
 
-  </body>
+		<!--<div class="subheading">
+			<center>
+				<div class="span6">
+					<h4>Subheading</h4>
+						<p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+					<h4>Subheading</h4>
+						<p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+					<h4>Subheading</h4>
+						<p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+				</div>
+
+				<div class="span6">
+					<h4>Subheading</h4>
+						<p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
+					<h4>Subheading</h4>
+						<p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
+					<h4>Subheading</h4>
+						<p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+				</div>
+				<hr>
+			</center>
+		</div>-->
+		
+			<div class="footer">
+					<p>©Intersection 2013</p>
+			</div>
+	
+		</div>
+
+	</body>
+
 </html>
